c48453b5438c2b1d40f13f002447c4476dd66111
Lennox Akpoduado <ejiro.akpoduado@6point6.co.uk>
2024-12-27 11:46:12 +0000

    Update AboutPage.vue and replace about.jpg: Enhance image styling in AboutPage.vue for better visual presentation and replace the existing about.jpg with an updated version. These changes improve the overall aesthetics and user experience of the About page.

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -10,7 +10,7 @@
                 innovative compositions that resonate deeply and elevate every project.
             </p>
             <div class="image-container mb-8">
-                <img src="/images/about.jpg" alt="Social Media" class=" w-[200px] h-[200px] fade-in " @load="fadeInImage" />
+                <img src="/images/about.jpg" alt="Social Media" class="w-[200px] h-[200px] object-cover fade-in" @load="fadeInImage" />
             </div>
             <div class="contact-section mt-12 mb-8">
                 <h2 class="text-xl font-kormelink italic mb-6">Get in Touch</h2>
